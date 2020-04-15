51d08e0e3776c38de141ab43304d4b74486c0c38
Emre Karaca <creagas@googlemail.com>
2020-04-15 16:44:03 +0200

    Added state

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,17 +1,31 @@
 import React from "react";
 
 export default class App extends React.Component {
+  state = {
+    note: ""
+  };
+
+  handleChange = event => {
+    this.setState({ note: event.target.value });
+  };
+
   render() {
     return (
       <div id="app">
         <h1>My Notepad - Class</h1>
         <div className="notepad">
           <label htmlFor="my-note">Editor:</label>
-          <textarea name="my-note" id="my-note" rows="10" />
+          <textarea
+            name="my-note"
+            id="my-note"
+            rows="10"
+            value={this.state.note}
+            onChange={this.handleChange}
+          />
         </div>
         <div className="viewer">
           <p className="viewer--heading">Viewer:</p>
-          <p id="viewer--text" />
+          <p id="viewer--text">{this.state.note}</p>
         </div>
       </div>
     );
--- src/AppFunctional.js
+++ src/AppFunctional.js
@@ -1,19 +1,25 @@
-// eslint-disable-next-line
 import React, { useState } from "react";
 
 export default function App() {
+  const [note, setNote] = useState("");
+  const handleChange = e => setNote(e.target.value);
+
   return (
     <div id="app">
       <h1>My Notepad - Functional</h1>
-
       <div className="notepad">
         <label htmlFor="my-note">Editor:</label>
-        <textarea name="my-note" id="my-note" rows="10" />
+        <textarea
+          name="my-note"
+          id="my-note"
+          rows="10"
+          value={note}
+          onChange={handleChange}
+        />
       </div>
-
       <div className="viewer">
         <p className="viewer--heading">Viewer:</p>
-        <p id="viewer--text" />
+        <p id="viewer--text">{note}</p>
       </div>
     </div>
   );
